b15a1586b1690ec26715026b7394ecbe225854d5
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-07-25 15:06:20 +0530

    connections
    -- connecting cloud db
    -- creating sample routes for
        -- profiles
        -- posts
        -- users
        -- authentication

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express")
+const connectDB = require("./config/db")
+const users = require("./routes/api/users")
+const auth = require("./routes/api/auth")
+const profile = require("./routes/api/profile")
+const posts = require("./routes/api/posts")
 
 const app = express()
+connectDB()
 
 app.get("/", (req, res) => res.send("API IS RUNNING"))
+
+//Routes
+app.use("/api/users", users)
+app.use("/api/auth", auth)
+app.use("/api/profile", profile)
+app.use("/api/posts", posts)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server Running On Port ${PORT}`))
